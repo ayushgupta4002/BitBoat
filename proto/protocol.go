6daccd7ce5345e9689ccea163024e75eb0312a49
Ayush Gupta <ayush4002gupta@gmail.com>
2024-12-26 18:47:52 +0530

    switched to binaries , proto test added

---
+++ proto/protocol.go
@@ -0,0 +1,90 @@
+package proto
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+type Command byte
+
+const (
+	Cmdnonce Command = iota
+	CmdSet
+	CmdGet
+	CmdDel
+)
+
+type CommandSet struct {
+	Key   []byte
+	Value []byte
+	TTL   int32
+}
+type CommandGet struct {
+	Key []byte
+}
+
+func (c *CommandSet) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, CmdSet)
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Key)))
+	binary.Write(buf, binary.LittleEndian, c.Key)
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Value)))
+	binary.Write(buf, binary.LittleEndian, c.Value)
+	binary.Write(buf, binary.LittleEndian, int32(c.TTL))
+	return buf.Bytes()
+}
+
+func (c *CommandGet) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, CmdGet)
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Key)))
+	binary.Write(buf, binary.LittleEndian, c.Key)
+	return buf.Bytes()
+}
+
+func parseCommand(r io.Reader) (any, error) {
+	var cmd Command
+	err := binary.Read(r, binary.LittleEndian, &cmd)
+	if err != nil {
+		return nil, err
+	}
+	switch cmd {
+	case CmdSet:
+		return parseSet(r), nil
+	case CmdGet:
+		return parseGet(r), nil
+	default:
+		return nil, fmt.Errorf("invalid command: %d", cmd)
+	}
+}
+
+func parseSet(r io.Reader) *CommandSet {
+	cmd := &CommandSet{}
+	var keyLen int32
+	binary.Read(r, binary.LittleEndian, &keyLen)
+	key := make([]byte, keyLen)
+	binary.Read(r, binary.LittleEndian, &key)
+	var valueLen int32
+	binary.Read(r, binary.LittleEndian, &valueLen)
+	value := make([]byte, valueLen)
+	binary.Read(r, binary.LittleEndian, &value)
+	var ttl int32
+	binary.Read(r, binary.LittleEndian, &ttl)
+	cmd.Key = key
+	cmd.Value = value
+	cmd.TTL = ttl
+	return cmd
+
+}
+
+func parseGet(r io.Reader) *CommandGet {
+	cmd := &CommandGet{}
+	var keyLen int32
+	binary.Read(r, binary.LittleEndian, &keyLen)
+	key := make([]byte, keyLen)
+	binary.Read(r, binary.LittleEndian, &key)
+	cmd.Key = key
+	return cmd
+}
---
+++ proto/protocol_test.go
@@ -0,0 +1,39 @@
+package proto
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCommand(t *testing.T) {
+
+	cmd := &CommandGet{
+		Key: []byte("foo"),
+	}
+	r := bytes.NewReader(cmd.Bytes())
+	CmdGet, err := parseCommand(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, CmdGet, cmd)
+}
+
+func TestSetCommand(t *testing.T) {
+
+	cmd := &CommandSet{
+		Key:   []byte("foo"),
+		Value: []byte("bar"),
+		TTL:   42,
+	}
+	r := bytes.NewReader(cmd.Bytes())
+	CmdSet, err := parseCommand(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, CmdSet, cmd)
+
+}
